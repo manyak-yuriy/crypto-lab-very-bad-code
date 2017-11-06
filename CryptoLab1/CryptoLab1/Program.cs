32ba1ff07e5abf4508a62a3a9dcec098705e77c4
manyak.yuriy <manyak.yuriy@gmail.com>
2017-11-06 13:13:53 +0200

    More arthur.txt filtering, add project

---
+++ CryptoLab1/CryptoLab1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLab1
+{
+    struct CharInfo
+    {
+        public char Character;
+        public int Count;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string allText = File.ReadAllText("Arthur.txt", Encoding.Default);
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (var character in allText)
+                if (charCount.ContainsKey(character))
+                {
+                    charCount[character]++;
+                }
+                else
+                {
+                    charCount[character] = 1;
+                }
+
+            List<CharInfo> charInfos =
+                charCount.Select(pair => new CharInfo()
+                    {
+                        Character = pair.Key,
+                        Count = pair.Value
+                    })
+                    .OrderByDescending(info => info.Count)
+                    .ToList();
+
+            foreach (var charInfo in charInfos)
+            {
+                Console.WriteLine("Char: {0}   Count: {1}", charInfo.Character, charInfo.Count);
+            }
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(allText);
+
+            Console.ReadKey();
+        }
+    }
+}
